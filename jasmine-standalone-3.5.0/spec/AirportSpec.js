959e3267d4ea522292f1fcafea2a236cb1d0556e
Daniel <demartinezgatell@gmail.com>
2019-12-10 14:48:22 +0000

    Airport and Plane spec and features added

---
+++ jasmine-standalone-3.5.0/spec/AirportSpec.js
@@ -0,0 +1,9 @@
+describe('Airport', function(){
+  var airport;
+  beforeEach(function(){
+    airport = new Airport();
+  });
+  it('has no planes by default', function(){
+    expect(airport.planes()).toEqual([]);
+  });
+});
